378e048e07514d4f201910e10caacd2b4344d3e6
Thomas SLOBODA <thomas@sloboda.fr>
2024-08-15 09:20:51 +0200

    feat(user-service): create user-service auth router

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express'
+import { handleAuthRoutes } from './routers/auth.router.ts'
+import bodyParser from 'body-parser'
 
 const HOST = process.env.HTTP_HOST || 'localhost'
 const PORT = parseInt(process.env.HTTP_PORT || '3000')
 const PROTOCOL = process.env.HTTP_PROTOCOL || 'http'
 
 const server = express()
+
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
+
+handleAuthRoutes(server)
 
 server.get('/', (req, res) => {
     res.send('Hello World')
---
+++ user-service/src/types/auth.ts
@@ -0,0 +1,15 @@
+export type AuthRequestRequest = {
+    email: string
+}
+
+export type AuthRequestDto = {
+    email: string
+}
+
+export type AuthVerifyRequest = {
+    token: string
+}
+
+export type VerifyTokenDto = {
+    token: string
+}
